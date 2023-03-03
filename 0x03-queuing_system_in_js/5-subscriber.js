1735674b5f9d5d9533f4fae02e806f1fd3de7953
KoketsoMalatji <koketsosean209@gmail.com>
2023-03-03 22:48:20 +0000

    system

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,21 +1,23 @@
-import redis from "redis";
+import redis from 'redis';
 
 const client = redis.createClient();
 
-client
-  .on("error", (error) => {
-    if (error) console.log(`Redis client not connected to the server: ${err}`);
-  })
-  .on("ready", () => {
-    console.log("Redis client connected to the server");
-  });
+client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
 
-client.subscribe("holberton school channel");
-
-client.on("message", (channel, message) => {
-  console.log(message);
-  if (message === "KILL_SERVER") {
-    client.unsubscribe(channel);
-    process.exit(0);
-  }
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+}).on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
 });
+
+client.subscribe('holberton school channel');
+client.on('message', (channel, message) => {
+    if (channel === 'holberton school channel') {
+        if (message === 'KILL SERVER') {
+            client.unsubscribe('holberton school channel');
+            console.log(message);
+            client.quit();
+        } else
+            console.log(message);
+    }
+})
